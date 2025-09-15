0e86d73fd9a5f393f07e38bbd4876b4732b8fbe9
Logger Key <loggerkey005@gmail.com>
2025-09-15 15:35:48 +0300

    Next Fixed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental:{
-    ppr:'incremental'
-  }
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+  },
 };
 
 export default nextConfig;
